4938d8891ea379b59652f1de5dcf20920fb3fd70
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-09-29 15:09:22 -0500

    ADD: se agrego version del simulador estable - falta mejorar el algoritmo de prestamo

--- src/ShortcodeController.php
+++ src/ShortcodeController.php
@@ -10,11 +10,11 @@ class ShortcodeController
     }
     public function index()
     {
-        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.56cda763.chunk.css'), '', '1.0.0');
+        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.50c133db.chunk.css'), '', '1.0.0');
 
         wp_enqueue_script('chuck_js_base', asset('/js/base.js'), '', '1.0.0', true);
-        wp_enqueue_script('chuck_js_react_1', assetStaticJSReact('2.ea22a037.chunk.js'), '', '1.0.0', true);
-        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.659d600d.chunk.js'), '', '1.0.0', true);
+        wp_enqueue_script('chuck_js_react_1', assetStaticJSReact('2.7ac078d6.chunk.js'), '', '1.0.0', true);
+        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.157777da.chunk.js'), '', '1.0.0', true);
 
         return view("shortcode", []);
     }
