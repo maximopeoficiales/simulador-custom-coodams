8814f6e5ea59c551db101750cd4671e6f8c2290d
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-12-05 02:20:11 -0500

    ADD: nueva caracteristica , se puede filtrar opciones por parametros en el shortcode

--- src/ShortcodeController.php
+++ src/ShortcodeController.php
@@ -10,11 +10,11 @@ class ShortcodeController
     }
     public function index($params)
     {
-        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.b4e0a18a.chunk.css'), '', '1.0.0');
+        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.2db46f9d.chunk.css'), '', '1.0.0');
 
         wp_enqueue_script('chuck_js_base', asset('/js/base.js'), '', '1.0.0', true);
-        wp_enqueue_script('chuck_js_react_1', assetStaticJSReact('2.4eca5775.chunk.js'), '', '1.0.0', true);
-        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.a15c7406.chunk.js'), '', '1.0.0', true);
+        wp_enqueue_script('chuck_js_react_1', assetStaticJSReact('2.7ac078d6.chunk.js'), '', '1.0.0', true);
+        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.dfe41408.chunk.js'), '', '1.0.0', true);
 
         return view("shortcode", [
             'show_only' => $params['show_only'] ?? 0
