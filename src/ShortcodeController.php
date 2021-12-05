1572bb306e40a08b58306bd0c3bfee25f6e8263c
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-12-05 03:00:23 -0500

    ADD: se modificaron clases css a español

--- src/ShortcodeController.php
+++ src/ShortcodeController.php
@@ -10,11 +10,11 @@ class ShortcodeController
     }
     public function index($params)
     {
-        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.2db46f9d.chunk.css'), '', '1.0.0');
+        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.bf2a25a2.chunk.css'), '', '1.0.0');
 
         wp_enqueue_script('chuck_js_base', asset('/js/base.js'), '', '1.0.0', true);
         wp_enqueue_script('chuck_js_react_1', assetStaticJSReact('2.7ac078d6.chunk.js'), '', '1.0.0', true);
-        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.09914765.chunk.js'), '', '1.0.0', true);
+        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.617ad331.chunk.js'), '', '1.0.0', true);
 
         return view("shortcode", [
             'show_only' => $params['show_only'] ?? 0
