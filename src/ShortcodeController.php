4d2224ba391c1060c5798d447efeaf07a146a176
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2021-12-05 03:46:12 -0500

    ADD: se agrego ultimas modificacioens en el css

--- src/ShortcodeController.php
+++ src/ShortcodeController.php
@@ -10,11 +10,11 @@ class ShortcodeController
     }
     public function index($params)
     {
-        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.bf2a25a2.chunk.css'), '', '1.0.0');
+        wp_enqueue_style('chuck_css_react', assetStaticCSSReact('main.a2ddedb7.chunk.css'), '', '1.0.0');
 
         wp_enqueue_script('chuck_js_base', asset('/js/base.js'), '', '1.0.0', true);
         wp_enqueue_script('chuck_js_react_1', assetStaticJSReact('2.7ac078d6.chunk.js'), '', '1.0.0', true);
-        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.c24cecc6.chunk.js'), '', '1.0.0', true);
+        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.d6e9ee1c.chunk.js'), '', '1.0.0', true);
 
         return view("shortcode", [
             'show_only' => $params['show_only'] ?? 0
