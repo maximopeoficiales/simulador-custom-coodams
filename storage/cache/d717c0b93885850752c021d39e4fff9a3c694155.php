99b0c2cc57ddcd98503454aacf52a91788d04068
Maximo Junior Apaza Chirhuana <maximopeoficiales@gmail.com>
2022-02-25 17:28:56 -0500

    ADD: plan de credito lisot

--- storage/cache/d717c0b93885850752c021d39e4fff9a3c694155.php
+++ storage/cache/d717c0b93885850752c021d39e4fff9a3c694155.php
@@ -1,7 +1,7 @@
 <h2>Tasas de Interes segun Categoria <?php echo e(get_option('libre_inversion')); ?></h2>
 <form action="" method="post">
 
-    <input type="hidden" name="updateOptionsSimulador" value="1">
+    <!-- <input type="hidden" name="updateOptionsSimulador" value="1">
     <div class="form-check">
         <label class="form-check-label">
             <input type="checkbox" class="form-check-input" name="sim_libre_inversion" value="1" <?php echo e(intval(get_option('sim_libre_inversion')) == 1 ? ' checked' : ''); ?>>
@@ -50,13 +50,13 @@
     <div class="form-group my-2">
         <label for="tasa_vehiculo_3">Vehiculo mas 120 meses:</label>
         <input type="number" name="tasa_vehiculo_3" id="tasa_vehiculo_3" step="0.1" class="form-control" placeholder="Ejm: 1.1" value="<?php echo e(get_option('tasa_vehiculo_3')); ?>">
-    </div>
+    </div> -->
     <div class="form-group my-2">
         <label for="tasa_plan_credito">Plan de Credito</label>
         <input type="number" name="tasa_plan_credito" id="tasa_plan_credito" step="0.1" class="form-control" placeholder="Ejm: 1.1" value="<?php echo e(get_option('tasa_plan_credito')); ?>">
     </div>
 
-    <hr>
+    <!-- <hr>
     <div class="form-group my-2">
         <label for="monto_max_libre_inversion">Monto Maximo Libre Inversion:</label>
         <input type="number" name="monto_max_libre_inversion" id="monto_max_libre_inversion" class="form-control" placeholder="Ejm: 5269451" value="<?php echo e(get_option('monto_max_libre_inversion')); ?>">
@@ -68,7 +68,7 @@
     <div class="form-group my-2">
         <label for="monto_max_vehiculo">Monto Maximo Vehiculo:</label>
         <input type="number" name="monto_max_vehiculo" id="monto_max_vehiculo" class="form-control" placeholder="Ejm: 45426300" value="<?php echo e(get_option('monto_max_vehiculo')); ?>">
-    </div>
+    </div> -->
     <div class="form-group my-2">
         <label for="monto_max_plan_credito">Monto Maximo Plan de Credito:</label>
         <input type="number" name="monto_max_plan_credito" id="monto_max_plan_credito" class="form-control" placeholder="Ejm: 45426300" value="<?php echo e(get_option('monto_max_plan_credito')); ?>">
