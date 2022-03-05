7089542b1ef6a333129567bb761b4810397c0e8c
Maximo Junior Apaza Chirhuana <maximopeoficiales>
2022-03-05 14:25:09 -0500

    ADD: correcion de tasas en plan de credito

--- src/ShortcodeController.php
+++ src/ShortcodeController.php
@@ -14,7 +14,7 @@ class ShortcodeController
 
         wp_enqueue_script('chuck_js_base', asset('/js/base.js'), '', '1.0.0', true);
         wp_enqueue_script('chuck_js_react_1', assetStaticJSReact('2.7ac078d6.chunk.js'), '', '1.0.0', true);
-        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.3d86509d.chunk.js'), '', '1.0.0', true);
+        wp_enqueue_script('chuck_js_react_2', assetStaticJSReact('main.a852eef6.chunk.js'), '', '1.0.0', true);
 
         return view("shortcode", [
             'show_only' => $params['show_only'] ?? 0
